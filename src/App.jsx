4fce2b9888b5ccbe385e64fc038236b2b3c11644
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2025-05-21 12:51:39 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { Link, NavLink, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import "./App.css";
 import Product from "./components/Product";
+import Home from "components/Home";
+import PageNotFound from "components/PageNotFound";
 
 const App = () => (
-  <Product />
+  <>
+    <div className="flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route exact component={Home} path="/" />
+      <Route exact component={Product} path="/product" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
 );
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
@@ -12,7 +12,9 @@ initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
