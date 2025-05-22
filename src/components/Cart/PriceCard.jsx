7499a080dfd46e17cb49e790a05ebecb02d860ac
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2025-05-22 17:09:17 +0530

    Implemented localization to smile cart

--- src/components/Cart/PriceCard.jsx
+++ src/components/Cart/PriceCard.jsx
@@ -1,3 +1,4 @@
+import { useTranslation, Trans } from "react-i18next";
 import React from 'react'
 import classNames from 'classnames'
 import { Typography, Button } from 'neetoui'
@@ -6,6 +7,8 @@ import useCartItemsStore from 'src/sources/useCartItemsStore'
 import routes from 'routes'
 
 const PriceCard = ({ totalMrp, totalOfferPrice }) => {
+
+  const { t } = useTranslation();
 
   const totalDiscounts = totalMrp - totalOfferPrice;
   const isDiscountPresent = gt(totalDiscounts, 0)
@@ -16,28 +19,37 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
   return (
     <div className='neeto-ui-rounded neeto-ui-border-black space-y-2 border p-3'>
       <Typography className={classNames("flex justify-between", {"line-through": isDiscountPresent})}>
-        Total MRP: <span>${totalMrp}</span>
+        <Trans
+          i18nKey="totalMrp"
+          components={{ typography: <span /> }}
+          values={{ mrp: totalMrp }}
+        />
       </Typography>
       {isDiscountPresent && (
         <>
           <Typography className='flex justify-between text-green-700 gap-4'>
-            Total discounts:{"  "}
-            <span>
-              ${totalDiscounts}({discountPercentage}%)
-            </span>
+            <Trans
+              components={{ span: <span /> }}
+              i18nKey="totalDiscounts"
+              values={{ discounts: totalDiscounts, discountPercentage }}
+            />
           </Typography>
           <Typography className="flex justify-between">
-            Total offer price: <span>${totalOfferPrice}</span>
+            <Trans
+              components={{ span: <span /> }}
+              i18nKey="offerPrice"
+              values={{ offerPrice: totalOfferPrice }}
+            />
           </Typography>
           <span className='neeto-ui-text-gray-500 text-sm'>
-            {itemsCount} item(s)
+            {t("itemCount", { count: itemsCount })}
           </span>
         </>
       )}
       <div className="flex flex-col items-center pt-4">
         <Button
           className="bg-neutral-800"
-          label="Buy now"
+          label={t("buyNow")}
           to={routes.checkout}
         />
       </div>
