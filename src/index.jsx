543ca7ba4242ea4556553691c94c07008ce801e9
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2025-05-22 16:39:34 +0530

    Initialized i18n

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import i18n from "components/commons/i18n";
 
 import App from "./App";
 import "./index.css";
