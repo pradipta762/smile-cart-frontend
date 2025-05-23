1c8f1504c8aca93ad10fcf1ea77452cd6b4cfc2b
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2025-05-23 12:38:38 +0530

    Adjusted ProductList component to use query parameter info

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,28 +1,50 @@
+import { filterNonNull } from 'neetocist';
+import useQueryParams from 'hooks/useQueryParams';
 import React, { useEffect, useState } from 'react'
 import productsApi from 'apis/products'
 import ProductListItem from './ProductListItem'
 import { Header, PageLoader, PageNotFound } from "components/commons";
 import { Input, NoData, Pagination } from 'neetoui';
 import { Search } from 'neetoicons';
-import { isEmpty, without } from 'ramda';
-import useDebounce from 'hooks/useDebounce';
+import { isEmpty, mergeLeft, without } from 'ramda';
 import { useFetchProducts } from 'hooks/reactQuery/useProductsApi';
 import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { buildUrl } from 'utils/url';
+import routes from 'routes';
+import useFuncDebounce from 'hooks/useFuncDebounce';
 
 const ProductList = () => {
 
-  const [searchKey, setSearchKey] = useState("")
-  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX)
+  const queryParams = useQueryParams();
+  const { page, pageSize, searchTerm = "" } = queryParams;
+  const [searchKey, setSearchKey] = useState(searchTerm);
 
-  const debouncedSearchKey = useDebounce(searchKey)
+  const history = useHistory();
 
   const productParams = {
-    searchTerm: debouncedSearchKey,
-    page: currentPage,
-    pageSize: DEFAULT_PAGE_SIZE,
+    searchTerm,
+    page: page || DEFAULT_PAGE_INDEX,
+    pageSize: pageSize || DEFAULT_PAGE_SIZE,
   }
 
   const { data: { products = [], totalProductsCount } = {}, isLoading } = useFetchProducts(productParams);
+
+  const handlePageNavigation = page =>
+    history.replace(
+      buildUrl(routes.products.index, mergeLeft({ page, pageSize: DEFAULT_PAGE_SIZE }))
+    )
+
+  const updateQueryParams = useFuncDebounce(value => {
+    const params = {
+      page: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+      searchTerm: value || null
+    }
+    setSearchKey(value)
+
+    history.replace(buildUrl(routes.products.index, filterNonNull(params)))
+  })
 
   if(isLoading) {
     return (
@@ -41,9 +63,9 @@ const ProductList = () => {
             prefix={<Search />}
             type='search'
             value={searchKey}
-            onChange={event => {
-              setSearchKey(event.target.value)
-              setCurrentPage(DEFAULT_PAGE_INDEX)
+            onChange={({ target: { value } }) => {
+              updateQueryParams(value)
+              setSearchKey(value)
             }}
           />
         }
@@ -62,10 +84,10 @@ const ProductList = () => {
       )}
       <div className="py-5 self-end">
         <Pagination
-          navigate={page => setCurrentPage(page)}
           count={totalProductsCount}
-          pageNo={currentPage || DEFAULT_PAGE_INDEX}
-          pageSize={DEFAULT_PAGE_SIZE}
+          navigate={handlePageNavigation}
+          pageNo={Number(page) || DEFAULT_PAGE_INDEX}
+          pageSize={Number(pageSize) || DEFAULT_PAGE_SIZE}
         />
       </div>
     </div>
